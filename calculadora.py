e4c9731364306a17be2b87763251586aa31daed9
Vicente Rama <vicente.rama@educa.madrid.org>
2025-04-09 21:31:12 +0200

    sumar

--- calculadora.py
+++ calculadora.py
@@ -11,7 +11,12 @@ def main():
         print("5. Salir")
         opcion = input("Elija una opción: ")
         print("Opción", opcion)
+        if opcion == "1":
+            numero1 = int(input("Ingrese un número: "))
+            numero2 = int(input("Ingrese otro número: "))
+            resultado = numero1 + numero2
+            print("La suma es: ", int(numero1) + int(numero2))
         seguir = opcion != "5"
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     main()
